11dd3b9704e207070d8cac3b540cf5c4ee90707a
Star Brilliant <m13253@hotmail.com>
2014-06-14 23:55:29 +0800

    Fix User-Agent problem

--- biligrab.py
+++ biligrab.py
@@ -73,7 +73,7 @@ def biligrab(url, *, oversea=False):
     logging.info('Calling Danmaku2ASS, converting to %s' % comment_out.name)
     danmaku2ass.Danmaku2ASS([comment_in], comment_out, video_size[0], video_size[1], font_face='SimHei', font_size=math.ceil(video_size[1]/21.6))
     logging.info('Invoking media player...')
-    player_process = subprocess.Popen(['mpv', '--ass', '--sub', comment_out.name, '--merge-files', '--autofit', '950x540', '--no-aspect']+media_urls)
+    player_process = subprocess.Popen(['mpv', '--http-header-fields', 'User-Agent: '+USER_AGENT, '--ass', '--sub', comment_out.name, '--merge-files', '--autofit', '950x540', '--no-aspect']+media_urls)
     player_process.wait()
     comment_out.close()
     return player_process.returncode
@@ -130,7 +130,7 @@ def main():
     if len(sys.argv) == 1:
         sys.argv.append('--help')
     parser = argparse.ArgumentParser()
-    parser.add_argument('--oversea', action='store_true', help='Enable bilibili oversea proxy')
+    parser.add_argument('-o', '--oversea', action='store_true', help='Enable bilibili oversea proxy')
     parser.add_argument('url', metavar='URL', nargs='+', help='Bilibili video page URL (http://www.bilibili.tv/av*)')
     args = parser.parse_args()
     if not checkenv():

eeac516bec3bede9646779304e52db3e581c5d7b
Star Brilliant <m13253@hotmail.com>
2014-06-15 15:33:31 +0800

    PEP8

--- biligrab.py
+++ biligrab.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python3
 
 # Biligrab
-# 
+#
 # Author: Beining@ACICFG https://github.com/cnbeining
 # Author: StarBrilliant https://github.com/m13253
-# 
+#
 # Biligrab is licensed under MIT licence
 # Permission has been granted for the use of Danmaku2ASS in Biligrab
-# 
+#
 # Copyright (c) 2014
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the “Software”), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -30,7 +30,7 @@
 
 
 import sys
-if sys.version_info < (3,0):
+if sys.version_info < (3, 0):
     sys.stderr.write('ERROR: Python 3.0 or newer version is required.\n')
     sys.exit(1)
 import argparse

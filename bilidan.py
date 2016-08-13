2f0a29d5aa8b4078a645e56aa3a17fc5108b1b50
pandada8 <pandada8@gmail.com>
2016-08-14 01:03:48 +0800

    update to new danmuku2ass api

--- bilidan.py
+++ bilidan.py
@@ -211,7 +211,7 @@ def biligrab(url, *, debug=False, verbose=False, media=None, comment=None, cooki
                 if k in d2aflags:
                     d2a_args[k] = j(d2aflags[k])
         try:
-            danmaku2ass.Danmaku2ASS([comment_in], comment_out, **d2a_args)
+            danmaku2ass.Danmaku2ASS([comment_in], 'Bilibili', comment_out, **d2a_args)
         except Exception as e:
             log_or_raise(e, debug=debug)
             logging.error('Danmaku2ASS failed, comments are disabled.')
